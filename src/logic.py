da1de95e2f6c4b622242d6932edee6a2b2517721
skl.progs <skl.progs@gmail.com>
2018-11-25 02:51:04 +0300

    DB; DefaultConfig

--- src/logic.py
+++ src/logic.py
@@ -1,15 +1,173 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import os
 import re
 import phrydy
+import sqlite3
 
 import shared    as sh
 import sharedGUI as sg
 
+PRODUCT = 'unmusic'
+VERSION = '1.0'
+
 import gettext, gettext_windows
 gettext_windows.setup_env()
-gettext.install('shared','./shared/resources/locale')
+gettext.install(PRODUCT,'../resources/locale')
+
+
+
+class DefaultConfig:
+    
+    def __init__(self):
+        self.values()
+        self.ihome   = sh.Home(app_name=PRODUCT)
+        self.Success = self.ihome.create_conf()
+    
+    def run(self):
+        self.db()
+    
+    def values(self):
+        self._fdb = ''
+    
+    def db(self):
+        f = 'logic.DefaultConfig.db'
+        if self.Success:
+            self._fdb = self.ihome.add_config(PRODUCT+'.db')
+            if self._fdb:
+                if os.path.exists(self._fdb):
+                    self.Success = sh.File(file=self._fdb).Success
+            else:
+                self.Success = False
+                sh.com.empty(f)
+        else:
+            sh.com.cancel(f)
+
+
+
+class Objects:
+    
+    def __init__(self):
+        self._default = self._db = None
+        
+    def db(self):
+        f = 'logic.Objects.db'
+        if not self._db:
+            path = self.default()._fdb
+            if self._default.Success:
+                self._db = DB(path=path)
+            else:
+                sh.log.append (f,_('WARNING')
+                              ,_('Wrong input data!')
+                              )
+                self._db = DB()
+        return self._db
+    
+    def default(self):
+        if not self._default:
+            self._default = DefaultConfig()
+            self._default.run()
+        return self._default
+
+
+
+class DB:
+    
+    def __init__(self,path):
+        self.Success = True
+        self._path = path
+        self.connect()
+        self.create_albums()
+        self.create_tracks()
+    
+    def create_tracks(self):
+        f = 'logic.DB.create_tracks'
+        if self.Success:
+            try:
+                # 9 columns by now
+                self.dbc.execute (
+                    'create table if not exists TRACKS (\
+                     ALBUMID   integer \
+                    ,TITLE     text    \
+                    ,NO        integer \
+                    ,LYRICS    text    \
+                    ,COMMENT   text    \
+                    ,SEARCH    text    \
+                    ,BITRATE   integer \
+                    ,LENGTH    integer \
+                    ,RATING    integer \
+                                                       )'
+                                 )
+            except Exception as e:
+                self.fail(f,e)
+        else:
+            sh.com.cancel(f)
+    
+    def create_albums(self):
+        f = 'logic.DB.create_albums'
+        if self.Success:
+            try:
+                # 8 columns by now
+                self.dbc.execute (
+                    'create table if not exists ALBUMS (\
+                     ALBUMID   integer autoinc \
+                    ,ALBUM     text    \
+                    ,ARTIST    text    \
+                    ,YEAR      integer \
+                    ,GENRE     text    \
+                    ,COUNTRY   text    \
+                    ,COMMENT   text    \
+                    ,SEARCH    text    \
+                                                       )'
+                                 )
+            except Exception as e:
+                self.fail(f,e)
+        else:
+            sh.com.cancel(f)
+    
+    def save(self):
+        f = 'logic.DB.save'
+        if self.Success:
+            sh.log.append (f,_('INFO')
+                          ,_('Save "%s"') % self._path
+                          )
+            try:
+                self.db.commit()
+            except Exception as e:
+                self.fail(f,e)
+        else:
+            sh.com.cancel(f)
+    
+    def fail(self,func,error):
+        self.Success = False
+        sh.objs.mes (func
+                    ,_('WARNING')
+                    ,_('Database "%s" has failed!\n\nDetails: %s') \
+                    % (self._path,str(error))
+                    )
+    
+    def close(self):
+        f = 'logic.DB.close'
+        if self.Success:
+            try:
+                self.dbc.close()
+            except Exception as e:
+                self.fail(f,e)
+        else:
+            sh.com.cancel(f)
+    
+    def connect(self):
+        f = 'logic.DB.connect'
+        if self.Success:
+            try:
+                self.db  = sqlite3.connect(self._path)
+                self.dbc = self.db.cursor()
+            except Exception as e:
+                self.fail(f,e)
+        else:
+            sh.com.cancel(f)
+
 
 
 class Track:
@@ -59,11 +217,7 @@ class Track:
         if self.Success:
             title = sh.Path(self.file).filename()
             if title:
-                title = re.sub('^\d+[\.]{0,1}[\s]{0,1}','',title)
-            if title:
-                return title
-            else:
-                return '?'
+                return re.sub('^\d+[\.]{0,1}[\s]{0,1}','',title)
         else:
             sh.com.cancel(f)
     
@@ -90,7 +244,9 @@ class Track:
                 if self.audio.title:
                     self._title = self.audio.title
                 else:
-                    self._title = self.extract_title()
+                    extracted = self.extract_title()
+                    if extracted:
+                        self._title = extracted
                 if self.audio.bitrate:
                     self._bitrate = self.audio.bitrate
                 if self.audio.length:
@@ -122,7 +278,12 @@ class Track:
 
 
 
+objs = Objects()
+
+
+
 if __name__ == '__main__':
     sg.objs.start()
-    Track('/tmp/test.mp3').summary()
+    objs.default()
+    objs.db()
     sg.objs.end()
