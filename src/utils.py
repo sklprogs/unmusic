f67ccc93e4846f2557f00fe80ee33486a02db6be
skl.progs <skl.progs@gmail.com>
2022-07-28 01:41:34 +0300

    utils.DeleteBad.get_sizes

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,72 @@ import logic as lg
 import gui as gi
 
 
+class DeleteBad:
+    
+    def __init__(self):
+        self.Success = True
+        # Generated with tests.BadRating.report
+        self.ids = ['593', '596', '597', '600', '609', '611', '613', '623', '642', '645', '651', '657', '659', '663', '664', '669', '685', '687', '691', '705', '712', '721', '723', '726', '735', '742', '743', '749', '751', '766', '774', '781', '786', '807', '820', '821', '828', '841', '852', '853', '854', '862', '866', '868', '872', '890', '901', '903', '904', '907', '912', '922', '933', '934', '950', '963', '968', '969', '978', '979', '994', '995', '997', '999', '1000', '1003', '1005', '1006', '1008', '1010', '1014', '1015', '1016', '1023', '1042', '1058', '1060', '1064', '1068', '1069', '1072', '1074', '1075', '1080', '1081', '1082', '1086', '1091', '1093', '1095', '1096', '1099', '1106', '1107', '1123', '1126', '1132', '1134', '1141', '1144', '1145', '1150', '1151', '1156', '1159', '1163', '1166', '1169', '1170', '1175', '1192', '1195', '1196', '1198', '1212', '1221', '1222', '1226', '1232', '1233', '1235', '1236', '1237', '1240', '1248', '1251', '1254', '1256', '1263', '1271', '1285', '1290', '1298', '1300', '1302', '1304', '1307', '1308', '1311', '1314', '1323', '1324', '1325', '1329', '1337', '1339', '1344', '1345', '1353', '1357', '1358', '1361', '1367', '1371', '1375', '1388', '1392', '1395', '1406', '1422', '1423', '1428', '1432', '1436', '1437', '1445', '1446', '1447', '1448', '1449', '1450', '1452', '1460', '1467', '1468', '1489', '1496', '1506', '1510', '1513', '1534', '1535', '1536', '1542', '1546', '1547', '1557', '1560', '1562', '1563', '1570', '1573', '1574', '1577', '1583', '1589', '1602', '1609', '1614', '1621', '1631', '1634', '1642', '1647', '1651', '1662', '1666', '1669', '1672', '1673', '1677', '1681', '1688', '1703', '1711', '1712', '1718', '1719', '1726', '1735', '1736', '1749', '1751', '1757', '1772', '1775', '1782', '1788', '1791', '1792', '1793', '1798', '1801', '1806', '1807', '1824', '1830', '1832', '1844', '1851', '1857', '1859', '1860', '1863', '1870', '1874', '1875', '1881', '1882', '1883', '1891', '1894', '1905', '1906', '1908', '1910', '1925', '1932', '1937', '1938', '1943', '1949', '1955', '1963', '1971', '1973', '1981', '1993', '1996', '1998', '2008', '2019', '2020', '2024', '2025', '2030', '2039', '2040', '2044', '2049', '2051', '2065', '2070', '2075', '2076', '2081', '2089', '2093', '2094', '2095', '2100', '2101', '2121', '2122', '2127', '2128', '2132', '2135', '2145', '2147', '2155', '2167', '2172', '2174', '2175', '2177', '2180', '2183', '2187', '5015', '5954', '6006', '7044', '7359', '7364', '7366', '7368', '7375', '7381', '7383', '7385', '7390', '7391', '7401', '7406', '7411', '7415', '7416', '7420', '7422', '7431', '7439', '7441', '7442', '7445', '7454', '7468', '7469', '7471', '7479', '7480', '7481', '7487', '7490', '7494', '7498', '7499', '7501', '7516', '7525', '7530', '7532', '7533', '7535', '7541', '7542', '7545', '7553', '7554', '7559', '7565', '7568', '7571', '7574', '7578', '7582', '7593', '7596', '7603', '7610', '7612', '7618', '7620', '7622', '7623', '7626', '7635', '7645', '7651', '7660', '7662', '7669', '7675', '7677', '7688', '7692', '7693', '7694', '7702', '7705', '7709', '7718', '7721', '7725', '7726', '7730', '7735', '7738', '7746', '7749', '7766', '7773', '7774', '7776', '7777', '7779', '7781', '7783', '7795', '7803', '7810', '7822', '7830', '7840', '7842', '7847', '7848', '7851', '7852', '7854', '7857', '7862', '7865', '7866', '7870', '7873', '7874', '7884', '7886', '7894', '7896', '7897', '7899', '7900', '7901', '9093', '9132', '10101']
+        self.del_dirs = []
+    
+    def get_sizes(self):
+        f = '[unmusic] utils.DeleteBad.get_sizes'
+        ihome = sh.Home('unmusic')
+        local = lg.objs.get_default().ihome.add_share(_('local collection'))
+        local = os.path.realpath(local)
+        external = lg.objs.default.ihome.add_share(_('external collection'))
+        external = os.path.realpath(external)
+        mobile = lg.objs.default.ihome.add_share(_('mobile collection'))
+        mobile = os.path.realpath(mobile)
+        total_size = 0
+        local_size = 0
+        external_size = 0
+        mobile_size = 0
+        gi.objs.get_progress().set_text(_('Calculate space to be freed'))
+        gi.objs.progress.show()
+        for i in range(len(self.ids)):
+            gi.objs.progress.update(i,len(self.ids))
+            path = os.path.join(local,self.ids[i])
+            if os.path.exists(path):
+                local_size += sh.Directory(path).get_size()
+                self.del_dirs.append(path)
+            path = os.path.join(external,self.ids[i])
+            if os.path.exists(path):
+                external_size += sh.Directory(path).get_size()
+                self.del_dirs.append(path)
+            path = os.path.join(mobile,self.ids[i])
+            if os.path.exists(path):
+                mobile_size += sh.Directory(path).get_size()
+                self.del_dirs.append(path)
+        gi.objs.progress.close()
+        total_size = sh.com.get_human_size(total_size,True)
+        local_size = sh.com.get_human_size(local_size,True)
+        external_size = sh.com.get_human_size(external_size,True)
+        mobile_size = sh.com.get_human_size(mobile_size,True)
+        mes = []
+        sub = _('Space to be freed:')
+        mes.append(sub)
+        sub = _('in total: {}').format(total_size)
+        mes.append(sub)
+        sub = _('external collection: {}').format(external_size)
+        mes.append(sub)
+        sub = _('local collection: {}').format(local_size)
+        mes.append(sub)
+        sub = _('mobile collection: {}').format(mobile_size)
+        mes.append(sub)
+        mes.append('')
+        sub = _('Do you really want to continue?')
+        mes.append(sub)
+        self.Success = sh.objs.get_mes(f,'\n'.join(mes)).show_question()
+        if not self.Success:
+            mes = _('Operation has been canceled by the user.')
+            sh.objs.get_mes(f,mes,True).show_info()
+    
+    def run(self):
+        self.get_sizes()
+
+
+
 class Image:
     
     def __init__(self):
@@ -391,7 +457,6 @@ com = Commands()
 if __name__ == '__main__':
     f = '[unmusic] utils.__main__'
     sh.com.start()
-    #com.extract_images()
-    com.alter()
-    #lg.objs.get_db().close()
+    #com.alter()
+    DeleteBad().run()
     sh.com.end()
