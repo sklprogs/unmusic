ae29d18f5a8e79a32da627566f77c1d00f09e154
skl.progs <skl.progs@gmail.com>
2024-04-28 14:48:25 +0300

    Add remaining entries

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-import sys
 import PyQt6
 import PyQt6.QtWidgets
+
+from skl_shared_qt.localize import _
+import skl_shared_qt.shared as sh
 
 
 class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
@@ -24,10 +26,20 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
     def set_labels(self):
         self.lbl_art = PyQt6.QtWidgets.QLabel()
         self.lbl_alb = PyQt6.QtWidgets.QLabel()
+        self.lbl_yer = PyQt6.QtWidgets.QLabel()
+        self.lbl_cnt = PyQt6.QtWidgets.QLabel()
+        self.lbl_com = PyQt6.QtWidgets.QLabel()
+        self.lbl_bit = PyQt6.QtWidgets.QLabel()
+        self.lbl_len = PyQt6.QtWidgets.QLabel()
     
     def set_entries(self):
         self.ent_art = PyQt6.QtWidgets.QLineEdit()
         self.ent_alb = PyQt6.QtWidgets.QLineEdit()
+        self.ent_yer = PyQt6.QtWidgets.QLineEdit()
+        self.ent_cnt = PyQt6.QtWidgets.QLineEdit()
+        self.ent_com = PyQt6.QtWidgets.QLineEdit()
+        self.ent_bit = PyQt6.QtWidgets.QLineEdit()
+        self.ent_len = PyQt6.QtWidgets.QLineEdit()
     
     def set_widgets(self):
         self.set_labels()
@@ -35,8 +47,13 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
         self.set_text()
     
     def set_text(self):
-        self.lbl_art.setText('Artist:')
-        self.lbl_alb.setText('Album:')
+        self.lbl_art.setText(_('Artist:'))
+        self.lbl_alb.setText(_('Album:'))
+        self.lbl_yer.setText(_('Year:'))
+        self.lbl_cnt.setText(_('Country:'))
+        self.lbl_com.setText(_('Comment:'))
+        self.lbl_bit.setText(_('Average bitrate:'))
+        self.lbl_len.setText(_('Total length:'))
     
     def set_gui(self):
         self.set_layout()
@@ -47,10 +64,20 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
     def add_labels(self):
         self.layout_.addWidget(self.lbl_art, 0, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
         self.layout_.addWidget(self.lbl_alb, 1, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.lbl_yer, 2, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.lbl_cnt, 3, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.lbl_com, 4, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.lbl_bit, 5, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.lbl_len, 6, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
     
     def add_entries(self):
         self.layout_.addWidget(self.ent_art, 0, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
         self.layout_.addWidget(self.ent_alb, 1, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.ent_yer, 2, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.ent_cnt, 3, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.ent_com, 4, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.ent_bit, 5, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.ent_len, 6, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
     
     def add_widgets(self):
         self.add_labels()
@@ -60,6 +87,7 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     f = '__main__'
+    import sys
     exe = PyQt6.QtWidgets.QApplication(sys.argv)
     app = AlbumEditor()
     app.show()
