cad187637dee844c5d893f2cd1e0d6dccda90ba7
skl.progs <skl.progs@gmail.com>
2024-04-29 12:14:15 +0300

    Add genre label-menu pair

--- src/gui.py
+++ src/gui.py
@@ -8,6 +8,18 @@ from skl_shared_qt.localize import _
 import skl_shared_qt.shared as sh
 
 
+#NOTE: Do not localize (being stored in DB)
+GENRES = ('?', 'Alternative Rock', 'Ambient', 'Black Metal', 'Blues'
+         ,'Brutal Death Metal', 'Chanson', 'Classical', 'Death Metal'
+         ,'Death Metal/Grindcore', 'Death/Black Metal'
+         ,'Death/Thrash Metal', 'Deathcore', 'Electronic', 'Ethnic', 'Game'
+         ,'Goregrind', 'Grindcore', 'Heavy Metal', 'Industrial Metal'
+         ,'Melodic Death Metal', 'Metal', 'Pop', 'Power Metal', 'Rap'
+         ,'Relaxation', 'Rock', 'Soundtrack', 'Technical Brutal Death Metal'
+         ,'Technical Death Metal', 'Thrash Metal', 'Vocal'
+         )
+
+
 class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
     
     def __init__(self, *args, **kwargs):
@@ -40,6 +52,7 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
         self.lbl_com = sh.Label(_('Comment:'))
         self.lbl_bit = sh.Label(_('Average bitrate:'))
         self.lbl_len = sh.Label(_('Total length:'))
+        self.lbl_gnr = sh.Label(_('Genre:'))
     
     def set_entries(self):
         self.ent_art = PyQt6.QtWidgets.QLineEdit()
@@ -50,9 +63,13 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
         self.ent_bit = PyQt6.QtWidgets.QLineEdit()
         self.ent_len = PyQt6.QtWidgets.QLineEdit()
     
+    def set_options(self):
+        self.opt_gnr = sh.OptionMenu(GENRES)
+    
     def set_widgets(self):
         self.set_labels()
         self.set_entries()
+        self.set_options()
     
     def configure(self):
         self.set_label_font_size()
@@ -64,6 +81,9 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
         self.setCentralWidget(self.parent)
         self.configure()
     
+    def add_options(self):
+        self.layout_.addWidget(self.opt_gnr.widget, 7, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+    
     def add_labels(self):
         self.layout_.addWidget(self.lbl_art.widget, 0, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
         self.layout_.addWidget(self.lbl_alb.widget, 1, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
@@ -72,6 +92,7 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
         self.layout_.addWidget(self.lbl_com.widget, 4, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
         self.layout_.addWidget(self.lbl_bit.widget, 5, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
         self.layout_.addWidget(self.lbl_len.widget, 6, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.layout_.addWidget(self.lbl_gnr.widget, 7, 0, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
     
     def add_entries(self):
         self.layout_.addWidget(self.ent_art, 0, 1, 1, 1, PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
@@ -85,6 +106,7 @@ class AlbumEditor(PyQt6.QtWidgets.QMainWindow):
     def add_widgets(self):
         self.add_labels()
         self.add_entries()
+        self.add_options()
         self.parent.setLayout(self.layout_)
 
 
