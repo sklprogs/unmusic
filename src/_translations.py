0b5a05ece78d30cbab701818d52a04290d2b9324
skl.progs <skl.progs@gmail.com>
2024-06-14 02:49:09 +0300

    Add _translations. Update localization

---
+++ src/_translations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from skl_shared_qt.localize import _
+
+''' These are translations that Poedit considers outdated and tries to remove
+    since they are stored in non-Python files (in JSON).
+    #TODO: Update to Poedit 3.3 and import JSON directly.
+'''
+
+_("Minimize the app window")
+_("Go to the previous album")
+_("Go to the next album")
+_("Go to the next unrated album")
+_("Go to the previous unrated album")
+_("Go to the first album")
+_("Go to the last album")
+_("Reload the current album")
+_("Save changes")
+_("Show tracks")
+_("Go to the album search field")
+_("Go to the album ID search field")
+_("Go to the track search field")
+_("Delete tracks with rating < 8")
