845211d451e516619dce0f61ca65f6abb5d73acd
skl.progs <skl.progs@gmail.com>
2025-02-09 12:08:53 +0300

    Based on passed arguments, load either Menu or AlbumEditor

--- src/unmusic.py
+++ src/unmusic.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import sys
+
 from skl_shared_qt.localize import _
-from skl_shared_qt.message.controller import Message
 from skl_shared_qt.graphics.root.controller import ROOT
 
 ''' If we use module-level variables instead of 'Objects', this must be done
@@ -12,14 +13,23 @@ from skl_shared_qt.graphics.root.controller import ROOT
 '''
 ROOT.get_root()
 
-from config import CONFIG
-from menu.controller import MENU
-
+from skl_shared_qt.message.controller import Message
 
 
 f = '[unmusic] unmusic.__main__'
+mes = _('Start with arguments: {}').format(' '.join(sys.argv))
+Message(f, mes).show_debug()
+
+from config import CONFIG
+
 if CONFIG.Success:
-    MENU.show()
+    if len(sys.argv) > 1 and sys.argv[1] == '--albums':
+        from album_editor.controller import ALBUM_EDITOR
+        ALBUM_EDITOR.reset()
+        ALBUM_EDITOR.show()
+    else:
+        from menu.controller import MENU
+        MENU.show()
 else:
     mes = _('Invalid configuration!')
     Message(f, mes, True).show_error()
