8400de837b1731cf865f6991144af9f594dce6a3
skl.progs <skl.progs@gmail.com>
2025-06-06 10:53:56 +0300

    tests.TestDB.get_many_bad_albums

--- src/tests.py
+++ src/tests.py
@@ -7,6 +7,9 @@ from skl_shared_qt.graphics.root.controller import ROOT
 
 ROOT.get_root()
 
+from config import PATHS
+from db import DB
+
 
 class Commands:
     
@@ -16,12 +19,48 @@ class Commands:
         for i in range(40):
             itracks.add()
         return itracks
+    
+    def get_many_bad_albums(self):
+        f = '[unmusic] tests.Commands.get_many_bad_albums'
+        idb = TestDB(PATHS.get_db())
+        Message(f, idb.get_many_bad_albums()).show_debug()
+        idb.close()
+        
+
+
+
+class TestDB(DB):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.albumid = 10806
+    
+    def get_many_bad_albums(self, rating=5, limit=5):
+        f = '[unmusic] tests.TestDB.get_many_bad_albums'
+        if not self.Success:
+            rep.cancel(f)
+            return
+        query = 'select distinct ALBUMID from TRACKS where RATING > 0 and RATING < ? order by ALBUMID desc'
+        # limit=0 provides an empty output
+        if limit:
+            query += ' limit ?'
+        try:
+            if limit:
+                self.dbc.execute(query, (rating, limit,))
+            else:
+                self.dbc.execute(query, (rating,))
+            result = self.dbc.fetchall()
+            if result:
+                return [item[0] for item in result]
+        except Exception as e:
+            self.fail(f, e)
 
 
 com = Commands()
 
 
 if __name__ == '__main__':
+    f = '[unmusic] tests.__main__'
     '''
     import gui.tests
     import gui.tracks
@@ -30,7 +69,10 @@ if __name__ == '__main__':
     app.set_gui(track.pnl_trk)
     app.show()
     '''
-    window = com.run_tracks()
-    window.show()
+    #window = com.run_tracks()
+    #window.show()
+    com.get_many_bad_albums()
+    mes = _('Goodbye!')
+    Message(f, mes).show_debug()
     ROOT.end()
 
